c7eb2cffbaea7da780e7b9d34dd67e2a0ab85800
alexis <costillaalexisemanuel14@gmail.com>
2025-02-15 20:14:51 -0300

    header listo

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,25 +2,37 @@ import React, { useEffect, useState } from "react";
 import "./Banner.css";
 
 function Banner() {
-    const [isScrolled, setIsScrolled] = useState(false);
+    const [headerFijo, setHeaderFijo] = useState(false);
+    const [cambioColor, setCambioColor] = useState(false); // Nuevo estado para el cambio de fondo
 
     useEffect(() => {
-        const handleScroll = () => {
+        const detectarScroll = () => {
             if (window.scrollY > 100) {
-                setIsScrolled(true);
+                setHeaderFijo(true);
             } else {
-                setIsScrolled(false);
+                setHeaderFijo(false);
+            }
+
+            if (window.scrollY > 950) { // Cuando el scroll supere los 150px
+                setCambioColor(true); // Cambiar el fondo
+            } else {
+                setCambioColor(false); // Restaurar el fondo original
             }
         };
 
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
+        window.addEventListener("scroll", detectarScroll);
+        return () => window.removeEventListener("scroll", detectarScroll);
     }, []);
 
     return (
         <main className="banner-container">
-            <header className={`header ${isScrolled ? "scrolled" : ""}`}>
+            <header className={`header ${headerFijo ? "scrolled" : ""} ${cambioColor ? "fondo-cambiado" : ""}`}>
                 <h1 className="banner-text">ANIA</h1>
+                <nav className={`enlaces ${headerFijo ? "visible" : "oculto"}`}>
+                    <a className="espacios" href="productos">Productos</a>
+                    <a className="espacios" href="Contacto">Contacto</a>
+                    <a className="espacios" href="Nosotros">Nosotros</a>
+                </nav>
             </header>
         </main>
     );
