61ac0c89c5814d06f055b3a18aaefb2047eea039
alexis <costillaalexisemanuel14@gmail.com>
2025-04-24 20:10:00 -0300

    listo el footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./footer.css";
-function Footer(){
+function Footer() {
     return (
-        <p>a</p>
+        <footer className="footer">
+
+            <div className="footer-left">
+                <h2 className="AniA">ANIA</h2>
+                <p>todos los derechos reservados ANIA 2025</p>
+            </div>
+
+
+            <div className="footer-right">
+                <h2>CONTACTO</h2>
+                <p>Gmail: bienesraices@gmail.com</p>
+                <p>Direct: Av. Calle Falsa 123</p>
+                <p>Tel: 11 6256-2657</p>
+            </div>
+        </footer>
+
+
     )
 }
 
